756af57854d2bbf89f69cea8a18980811305225c
itsammar7 <maansaridot@gmail.com>
2019-09-08 23:26:10 +0500

    server.js edited

--- server.js
+++ server.js
@@ -46,10 +46,10 @@ app.use('/api/profile', profile);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    //Set status folder
+    // Set static folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
