d96744381b68b681226c1de4dc59c9da94a7f7f1
iAmmar7 <maansaridot@gmail.com>
2019-09-09 15:19:31 +0500

    prepare to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://ammar:hello1@ds123454.mlab.com:23454/courier',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require('./keys_prod')
+} else {
+  module.exports = require('./keys_dev')
+}
